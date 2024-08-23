941f37129fdc023ded75566c404a648d6ae4de9d
Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2024-08-24 04:17:33 +0800

    feat: change `darkToggle` icon & add transition

--- app/components/layout/main/DarkToggle.vue
+++ app/components/layout/main/DarkToggle.vue
@@ -12,10 +12,49 @@ useHead({
 function toggleDark() {
   color.preference = color.value === 'dark' ? 'light' : 'dark'
 }
+
+function handleToggle(event: MouseEvent) {
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+  if (!document.startViewTransition) {
+    toggleDark()
+    return
+  }
+  const transition = document.startViewTransition(async () => {
+    toggleDark()
+  })
+
+  transition.ready.then(() => {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ]
+    document.documentElement.animate(
+      {
+        clipPath: color.value === 'dark' ? [...clipPath].reverse() : clipPath,
+      },
+      {
+        duration: 360,
+        easing: 'ease-in',
+        pseudoElement: color.value === 'dark'
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      },
+    )
+  })
+}
 </script>
 
 <template>
-  <button class="!outline-none" @click="toggleDark">
-    <div class="i-carbon-sun dark:i-carbon-moon" />
+  <button class="!outline-none" @click="handleToggle">
+    <div
+      class="i-ri-sun-fill dark:i-ri-moon-fill"
+      hover="scale-110"
+      transition="all duration-200"
+    />
   </button>
 </template>
