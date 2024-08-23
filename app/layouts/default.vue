e49f4acda5b97868824b716814c893399d30d2bf
Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2024-08-24 03:52:53 +0800

    feat: base layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,18 @@
 <template>
-  <main class="px-10 py-20 text-center">
-    <slot />
-    <Footer />
-    <div class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
+  <div h-screen flex flex-col>
+    <div class="w-full flex items-center justify-between p-4 md:hidden">
+      <LayoutMainDarkToggle />
     </div>
-  </main>
+
+    <div class="flex flex-1">
+      <div class="hidden w-64 p-4 md:block">
+        <div absolute bottom-4 left-4>
+          <LayoutMainDarkToggle />
+        </div>
+      </div>
+      <div class="flex-1 overflow-auto p-4 md:mx-auto md:max-w-4xl">
+        <slot />
+      </div>
+    </div>
+  </div>
 </template>
