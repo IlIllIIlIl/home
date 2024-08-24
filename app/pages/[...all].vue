79fe73c5710d13b962e6dcb558fb648138590694
Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2024-08-25 02:03:31 +0800

    feat: add 404 page

--- app/pages/[...all].vue
+++ app/pages/[...all].vue
@@ -3,14 +3,24 @@ const router = useRouter()
 </script>
 
 <template>
-  <main p="x4 y10" text="center teal-700 dark:gray-200">
-    <div text-4xl>
-      <div i-carbon-warning inline-block />
+  <main h-full flex flex-col items-center justify-center>
+    <div text-6xl>
+      <div i-ri-inbox-2-fill inline-block />
     </div>
-    <div>Not found</div>
+    <h1 text-4xl font-black>
+      404
+    </h1>
+    <div mt-2 text-xl text-gray-800 dark:text-gray-300>
+      这里什么也没有
+    </div>
     <div>
-      <button text-sm btn m="3 t8" @click="router.back()">
-        Back
+      <button
+        mt-8 text-lg
+        text="blue-700 hover:blue-500 active:blue-800 dark:blue-500 dark:hover:blue-400 dark:active:blue-700"
+        transition-all duration-360
+        @click="router.back()"
+      >
+        返回
       </button>
     </div>
   </main>
