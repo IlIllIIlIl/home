99d4b9900274108a7c53cfba9ac2493acd0cdb59
Lee <cnlizerui@gmail.com>
2025-10-02 23:07:49 +0800

    feat: enhance base layout and components

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig, presetAttributify, presetIcons, presetTypography, presetWind4, transformerDirectives, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
+  shortcuts: {
+    'bg-background': 'bg-neutral-50 dark:bg-neutral-950',
+    'text-foreground': 'text-neutral-700 dark:text-neutral-200',
+    'text-muted': 'text-neutral-500 dark:text-neutral-400',
+  },
   presets: [
     presetWind4({
       dark: 'media',
