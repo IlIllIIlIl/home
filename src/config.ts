0108d3f39a2469d081a99e24b499e02c6d100b60
Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2026-02-20 00:19:53 +0800

    feat: writings page

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@ export const config = {
   name: "lipsum",
   title: "lipsum - the personal website",
   navLinks: [
-    "/projects",
-    "/contact"
+    "/writings",
   ]
 }
---
+++ src/utils/format.ts
@@ -0,0 +1,9 @@
+export function formatDate(date: Date) {
+  return new Date(date).toLocaleDateString('zh-Hant-u-nu-hanidec', {
+    year: '2-digit',
+    month: 'long',
+  })
+    .replace(/一〇/g, '十')
+    .replace(/一一/g, '十一')
+    .replace(/一二/g, '十二')
+}
