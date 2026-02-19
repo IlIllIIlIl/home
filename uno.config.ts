9987fe12a7f3d7c4c697b6248d74f43f5937bc54
Lee <lee.git@preblocks.com>
2026-02-20 03:42:40 +0800

    feat: name

--- uno.config.ts
+++ uno.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
     'bg-background': 'bg-neutral-50 dark:bg-neutral-950',
     'text-foreground': 'text-neutral-700 dark:text-neutral-200',
     'text-muted': 'text-neutral-500 dark:text-neutral-400',
-    'typography': 'prose prose-neutral dark:prose-invert prose-h1:font-serif prose-h2:font-serif prose-h3:font-serif prose-blockquote:font-mono prose-a:link font-sans',
+    'typography': ['prose prose-neutral dark:prose-invert', 'prose-h1:font-serif prose-h2:font-serif prose-h3:font-serif prose-blockquote:font-mono prose-a:link', 'font-sans wrap-break-word'],
     'divide': 'divide-y not-dark:divide-neutral-200 dark:divide-neutral-900',
-    'link': 'underline decoration-offset-3',
+    'link': 'underline decoration-offset-3 decoration-current/50 hover:decoration-current transition-colors',
   },
   presets: [
     presetWind4({
