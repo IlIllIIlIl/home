4811a180df301e88249b69ef8a01e254013391ae
Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2026-02-20 03:42:40 +0800

    feat: name

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,21 @@
 export const config = {
-  name: "lipsum",
-  title: "lipsum - the personal website",
+  name: "Ze Lee",
+  title: "lipsum",
   navLinks: [
     "/writings",
-  ]
+  ],
+  characters: [
+    ['Traditional', '李澤'],
+    ['Simplified', '李泽'],
+    ['Shinjitai', '李沢'],
+  ],
+  transcriptions: [
+    ['Bopomofo', 'ㄌㄧˇ ㄗㄜˊ'],
+    ['Hanyu Pinyin', 'Lǐ Zé'],
+    ['Wade-Giles', 'Li Tsê'],
+    ['Gwoyeu Romatzyh', 'Lii Tzer'],
+    // ['Jyutping', 'lei5 zak1'],
+    // ['Yale', 'léih jaahk'],
+    // ['IPA', 'liː tsɛk'],
+  ],
 }
