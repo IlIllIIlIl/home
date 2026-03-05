09b5b131b3bf28d82bd18568c3d2429c10a40da8
Lee <lee.git@preblocks.com>
2026-03-05 22:09:28 +0800

    chore: update typography

--- uno.config.ts
+++ uno.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     'bg-background': 'bg-neutral-50 dark:bg-neutral-950',
     'text-foreground': 'text-neutral-700 dark:text-neutral-200',
     'text-muted': 'text-neutral-500 dark:text-neutral-400',
-    'typography': ['prose prose-neutral dark:prose-invert', 'prose-h1:font-serif prose-h2:font-serif prose-h3:font-serif prose-blockquote:font-mono prose-a:link', 'font-sans wrap-break-word'],
+    'typography': ['prose prose-neutral dark:prose-invert', 'prose-h1:font-serif prose-h2:font-serif prose-h3:font-serif prose-h4:font-serif prose-strong:font-serif prose-blockquote:font-mono prose-a:link', 'font-sans text-pretty warp-normal break-normal'],
     'divide': 'divide-y not-dark:divide-neutral-200 dark:divide-neutral-900',
     'link': 'underline decoration-offset-3 decoration-current/50 hover:decoration-current transition-colors',
   },
@@ -20,7 +20,7 @@ export default defineConfig({
       provider: 'none',
       fonts: {
         sans: ['Geist Mono', 'Chocolate Classical Sans', 'Noto Sans KR', 'Noto Sans JP', 'Noto Sans TC', 'IBM Plex Sans TC'],
-        serif: ['Noto Serif JP', 'Noto Serif KR', 'Noto Serif TC'],
+        serif: ['Noto Serif KR', 'Noto Serif JP', 'Noto Serif TC', 'Noto Serif SC'],
         mono: ['Geist Mono', 'JetBrains Mono', 'IBM Plex Mono', 'Noto Sans Mono', 'LXGW WenKai Mono TC'],
       },
     }),
